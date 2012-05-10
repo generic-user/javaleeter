cd19856edb1e98c6b639ce07f8e832510e38bbeb
BattoorCjorn <orhiannono@gmail.com>
2012-05-10 12:39:23 +0300

    Formating changed

--- core.js
+++ core.js
@@ -77,63 +77,79 @@ new Array("z", "Z", "2", "7_", "-/_", "%", ">_", "s", "~/_", "-\\_", "-|_"));
 var alfabethaIndex = new Array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p,', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
 
 function javaleeter(str_text, maxlevel, weight) {
-        "use strict";
-	//Input test, level should always higher than 2
-  if (maxlevel === undefined) {throw "javaleeter encoutered a bad argument: maxlevel is undefined.";}
+    "use strict";
+    //Input test, level should always higher than 2
+    if (maxlevel === undefined) {
+        throw "javaleeter encoutered a bad argument: maxlevel is undefined.";
+    }
 
-  if (weight === undefined) {throw "javaleeter encoutered a bad argument: weight is undefined.";}
+    if (weight === undefined) {
+        throw "javaleeter encoutered a bad argument: weight is undefined.";
+    }
 
-  if (str_text === undefined) {throw "javaleeter encoutered a bad argument: str_text is undefined.";}
+    if (str_text === undefined) {
+        throw "javaleeter encoutered a bad argument: str_text is undefined.";
+    }
 
-  if (!javaleeterIsNumber(maxlevel)) {throw "javaleeter encoutered a bad argument: maxlevel should be an number.";}
+    if (!javaleeterIsNumber(maxlevel)) {
+        throw "javaleeter encoutered a bad argument: maxlevel should be an number.";
+    }
 
-  if (!javaleeterIsNumber(weight)) {throw "javaleeter encoutered a bad argument: weight should be an number.";}
+    if (!javaleeterIsNumber(weight)) {
+        throw "javaleeter encoutered a bad argument: weight should be an number.";
+    }
 
-	//If number is no integer we need to make it an integer.
-	maxlevel = Math.floor(maxlevel);
+    //If number is no integer we need to make it an integer.
+    maxlevel = Math.floor(maxlevel);
 
-	//if number is lower than 2 we make it 2 ourselfs
-  if (maxlevel < 2) {maxlevel = 2;}
+    //if number is lower than 2 we make it 2 ourselfs
+    if (maxlevel < 2) {
+        maxlevel = 2;
+    }
 
-	//if a bad weight value has been supplyed, use default value
-  if (weight < 0 || weight > 1) {weight = 0.6;}
+    //if a bad weight value has been supplyed, use default value
+    if (weight < 0 || weight > 1) {
+        weight = 0.6;
+    }
 
-	var index = 0;
+    var index = 0;
 
-	//This var will hold the newly created text
-	var convertedText = "";
-	//do this while we have chars in the string
-	while (index !== str_text.length) {
+    //This var will hold the newly created text
+    var convertedText = "";
+    //do this while we have chars in the string
+    while (index !== str_text.length) {
 
-		//find out what char we are dealing with (we make no diff between lower or uppercase!)
-		var alfabethNumber = alfabethaIndex.indexOf(str_text[index].toLowerCase());
-		//Calculate weight
-		var counterWeight = Math.random();
+        //find out what char we are dealing with (we make no diff between lower or uppercase!)
+        var alfabethNumber = alfabethaIndex.indexOf(str_text[index].toLowerCase());
+        //Calculate weight
+        var counterWeight = Math.random();
 
-		//If is is a letter from the alfabet (alfabethNumber != -1) and the counter weight is smaller then te weight
-		if (alfabethNumber !== -1 && counterWeight < weight) {
-			var topLimit = replacement[alfabethNumber].length - 1;
+        //If is is a letter from the alfabet (alfabethNumber != -1) and the counter weight is smaller then te weight
+        if (alfabethNumber !== -1 && counterWeight < weight) {
+            var topLimit = replacement[alfabethNumber].length - 1;
 
-			//set limit
-                  if (topLimit > maxlevel){ topLimit = maxlevel;}
-			//roll the dice
-			var dice = Math.floor(Math.random() * (topLimit + 1));
-			var selected = replacement[alfabethNumber][dice];
-			//Now we now what to replace it with, add it to the converted text
-			convertedText += selected;
+            //set limit
+            if (topLimit > maxlevel) {
+                topLimit = maxlevel;
+            }
+            //roll the dice
+            var dice = Math.floor(Math.random() * (topLimit + 1));
+            var selected = replacement[alfabethNumber][dice];
+            //Now we now what to replace it with, add it to the converted text
+            convertedText += selected;
 
-			//check next letter
-			index++;
-		} else {
-			//It is not a char that can be leeted, so add it to the converted text and add one to the index counter
-			convertedText += str_text[index++];
-		}
+            //check next letter
+            index++;
+        } else {
+            //It is not a char that can be leeted, so add it to the converted text and add one to the index counter
+            convertedText += str_text[index++];
+        }
 
-	}
-	return convertedText;
+    }
+    return convertedText;
 }
 
 function javaleeterIsNumber(n) {
-        "use strict";
-	return !isNaN(parseFloat(n)) && isFinite(n);
+    "use strict";
+    return !isNaN(parseFloat(n)) && isFinite(n);
 }
